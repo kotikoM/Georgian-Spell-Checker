8c1a284bab29a24e71eacb03e3ad8463e7bd8b06
Konstantine Machavariani <kotiko2004@gmail.com>
2025-12-23 15:27:04 +0400

    add test words

--- src/inference.py
+++ src/inference.py
@@ -1,12 +1,13 @@
 import torch
 
-from src.model import CharSeq2Seq, vocab_size
+from src.model import CharSeq2Seq
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = CharSeq2Seq(vocab_size)
-model.load_state_dict(torch.load("georgian_spellcheck_model_final.pth", map_location=device))
+model = CharSeq2Seq()
+model.load_state_dict(torch.load("../trained/georgian_spellcheck_model_final.pth", map_location=device))
 
+# for checkpoint loading
 # checkpoint = torch.load("checkpoint_epoch_1.pth", map_location=device)
 # model.load_state_dict(checkpoint["model_state_dict"])
 
@@ -14,14 +15,36 @@ model.to(device)
 print("Model loaded")
 
 test_words = [
-    "თბილისი",
-    "მთვარეე",
-    "ცამოდი",
-    "ქიგნი",
+    "გამარჯობა", # correct
+    "რობოტი",  # correct
+    "გიორგი",  # correct
+    "გზა", # correct
+    "მწვრთნელი", # correct
+    "იასამანი",  # correct
+    "ჰიდროელექტროსადგური",  # correct
 
+    "გამარჯობ", # deleted ი
+    "მზეზ",  # deleted ი
+    "სოკ",  # deleted ო
+    "გამ",  # deleted ო
+    "ფანჯრა",  # deleted ა
+
+    "მთვარეე", # duplicated ე
+    "მზეზზე",  # duplicated ზ
+    "მგელიი",  # duplicated ი
+    "წიგნნნი",  # triplicated ნ
+    "გოგონნა",  # duplicated ნ
+
+    "ცამოდი", # appended ც
+    "დავთით", # swapped თ ვ
+    "ყტემალი", # swapped ტ ყ
+    "მოტოციკკეტი", # swapped ლ to კ
+
+    "სოო",  # many words could form, model chose სოლო
+    "" # just curious on output for empty string, model chose ალ
 ]
 
 print("Spellcheck results:\n")
 for w in test_words:
     corrected = model.correct_word(w)
-    print(f"{w} -> {corrected}")
+    print(f"-    {w} -> {corrected}")
